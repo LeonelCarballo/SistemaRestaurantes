141a61e8c636c94e2cade7d6e99cc5e70bbd65aa
LeonelCarballo <leonel.carballo235352@potros.itson.edu.mx>
2025-03-28 16:30:50 -0700

    Creación proyectos

---
+++ SistemaRestaurantesNegocio/src/main/java/com/mycompany/sistemarestaurantesnegocio/SistemaRestaurantesNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemarestaurantesnegocio;
+
+/**
+ *
+ * @author leoca
+ */
+public class SistemaRestaurantesNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ SistemaRestaurantesPersistencia/src/main/java/com/mycompany/sistemarestaurantespersistencia/SistemaRestaurantesPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemarestaurantespersistencia;
+
+/**
+ *
+ * @author leoca
+ */
+public class SistemaRestaurantesPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
