f665f779319d93dbc629d60bfb96c792c7bde2c9
LeonelCarballo <leonel.carballo235352@potros.itson.edu.mx>
2025-03-28 17:27:32 -0700

    Primer mapeo comanda y creacion enums

--- SistemaRestaurantesDominio/src/main/java/entidades/Comanda.java
+++ SistemaRestaurantesDominio/src/main/java/entidades/Comanda.java
@@ -5,22 +5,53 @@
 package entidades;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
  * @author leoca
  */
 @Entity
+@Table(name = "comandas")
 public class Comanda implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    @Column(name = "id_comanda")
     private Long id;
+    
+    @Column (name = "folio", nullable = false , length = 15)
+    private String folio;
+    
+    @Enumerated (EnumType.STRING)
+    @Column (name = "estado" , nullable = false)
+    private EstadoComanda estadoComanda; 
+    
+    @Temporal (TemporalType.TIMESTAMP)
+    @Column (name = "fechaHora_creacion" , nullable = false)
+    private Calendar fechaHora_creacion;
+    
+    public Comanda() {
+    }
+
+    public Comanda(Long id, String folio, EstadoComanda estadoComanda, Calendar fechaHora_creacion) {
+        this.id = id;
+        this.folio = folio;
+        this.estadoComanda = estadoComanda;
+        this.fechaHora_creacion = fechaHora_creacion;
+    }
 
     public Long getId() {
         return id;
@@ -29,6 +60,32 @@ public class Comanda implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getFolio() {
+        return folio;
+    }
+
+    public void setFolio(String folio) {
+        this.folio = folio;
+    }
+
+    public EstadoComanda getEstadoComanda() {
+        return estadoComanda;
+    }
+
+    public void setEstadoComanda(EstadoComanda estadoComanda) {
+        this.estadoComanda = estadoComanda;
+    }
+
+    public Calendar getFechaHora_creacion() {
+        return fechaHora_creacion;
+    }
+
+    public void setFechaHora_creacion(Calendar fechaHora_creacion) {
+        this.fechaHora_creacion = fechaHora_creacion;
+    }
+    
+    
 
     @Override
     public int hashCode() {
