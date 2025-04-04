2475d1cfa0c7203c6b5a00f5435420fe8f1f635c
dreamywiwo <danacg11@hotmail.com>
2025-04-03 22:33:35 -0700

    Arreglo mesas y Comandas DAO

---
+++ SistemaRestaurantesPersistencia/src/main/java/wonderland/sistemarestaurantespersistencia/IComandasDAO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package wonderland.sistemarestaurantespersistencia;
+
+import wonderland.sistemarestaurantesdominio.Comanda;
+import wonderland.sistemarestaurantesdominio.dtos.NuevaComandaDTO;
+
+/**
+ *
+ * @author Dana Chavez
+ */
+public interface IComandasDAO {
+    public abstract Comanda crearNuevarComanda(NuevaComandaDTO nuevaComanda);
+    
+    //public abstract Comanda editarComanda(ComandaDTO comandaDTO);
+}
